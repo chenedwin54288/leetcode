318f644f07cec3606a35e93ec5b02085e8982f96
chenedwin54288 <109391763+chenedwin54288@users.noreply.github.com>
2026-03-20 02:07:52 +0100

    Time: 75 ms (94.68%) | Memory: 20.7 MB (49.69%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -10,6 +10,11 @@ class Solution(object):
         right_pointer = len_height - 1
         max_area = 0
 
+        # Below is an O(n) solution
+        # - if you think about it, your container can only contain 
+        #   min{height[l_p], height[r_p]} * (r_p - l_p)
+        # - so if you take two pointers from both side and gradually compute
+        #   the inner containers, you could get the maximum possible container
         while(left_pointer != right_pointer):
             if height[left_pointer] < height[right_pointer]:
                 max_area = max(max_area, height[left_pointer] * (right_pointer - left_pointer))
