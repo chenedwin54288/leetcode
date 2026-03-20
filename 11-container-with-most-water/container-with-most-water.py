fda9f3fda35c2917fed41fb9f243a7aff45b2a94
chenedwin54288 <109391763+chenedwin54288@users.noreply.github.com>
2026-03-20 02:03:51 +0100

    Time: 83 ms (91.01%) | Memory: 20.8 MB (15.78%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+        len_height = len(height)
+        left_pointer = 0
+        right_pointer = len_height - 1
+        max_area = 0
+
+        while(left_pointer != right_pointer):
+            if height[left_pointer] < height[right_pointer]:
+                max_area = max(max_area, height[left_pointer] * (right_pointer - left_pointer))
+                left_pointer += 1
+            elif height[left_pointer] > height[right_pointer]:
+                max_area = max(max_area, height[right_pointer] * (right_pointer - left_pointer))
+                right_pointer -= 1
+            else:
+                max_area = max(max_area, height[left_pointer] * (right_pointer - left_pointer))
+                left_pointer += 1
+
+        return max_area
+        
