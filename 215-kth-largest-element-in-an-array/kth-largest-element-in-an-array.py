3f350a79b4c31b93ec4915f60e22efd7f238f7dc
chenedwin54288 <109391763+chenedwin54288@users.noreply.github.com>
2026-03-22 01:47:03 +0100

    Time: 631 ms (30.82%) | Memory: 19.6 MB (67.61%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,15 +1,29 @@
 import heapq
+# class Solution(object):
+#     def findKthLargest(self, nums, k):
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: int
+#         """
+#         temp = [-n for n in nums]
+#         heapq.heapify(temp)
+#         k_th_largest = 0
+#         for i in range(k):
+#             k_th_largest = -heapq.heappop(temp)
+#         return k_th_largest
+
+
+
 class Solution(object):
     def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        temp = [-n for n in nums]
-        heapq.heapify(temp)
-        k_th_largest = 0
-        for i in range(k):
-            k_th_largest = -heapq.heappop(temp)
-        return k_th_largest
+        # Maintain a min-heap of size k
+        min_heap = []
+        for num in nums:
+            heapq.heappush(min_heap, num)
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        
+        # The root is the k-th largest
+        return min_heap[0]
         
