a3dc403a0b3b1112b95c69ed9b88db2c3033d83b
chenedwin54288 <109391763+chenedwin54288@users.noreply.github.com>
2026-03-23 17:05:10 +0100

    Time: 17 ms (66.23%) | Memory: 12.5 MB (74.10%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -6,19 +6,20 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         result = []
-        def find_sum(p_sum, l):
+        def find_sum(p_sum, l, index):
             if p_sum < target:
-                for j in candidates:
-                    find_sum(p_sum+j, l+[j])
+                for j in range(0, len(candidates)):
+                    if index <= j:
+                         find_sum(p_sum + candidates[j], l + [candidates[j]], j)
                 return 
             elif p_sum > target:
                 return
             else: # p_sum == target
-                sorted_l = sorted(l)
+                # sorted_l = sorted(l)
 
-                # very inefficient, ensuring uniqueness
-                if sorted_l not in result:
-                    result.append(sorted_l)
+                # # very inefficient, ensuring uniqueness
+                # if sorted_l not in result:
+                result.append(l)
         
-        find_sum(0, [])
+        find_sum(0, [], 0)
         return result
