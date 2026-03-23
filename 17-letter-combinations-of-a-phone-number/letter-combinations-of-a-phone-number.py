d6551fce80c748d66b79d1ffc0dab17efe2483f2
chenedwin54288 <109391763+chenedwin54288@users.noreply.github.com>
2026-03-23 15:23:50 +0100

    Time: 0 ms (100.00%) | Memory: 12.3 MB (78.68%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -19,6 +19,17 @@ class Solution(object):
             if curr_dig == len(digits):
                 result.append(p_str)
                 return p_str
+            
+            # Time Complexity O(3^N * 4^M)
+            # - N is the number of digits with 3 chers
+            # - M is the number of digits with 4 chars
+            # For instance when you press "23"
+            # [a,b,c] x [d,e,f] => 9 combinations also the time complexity
+
+            # suppose digits == "23"
+            # "a" -> ["e", "f", "g"]
+            # "b" -> ["e", "f", "g"]
+            # "c" -> ["e", "f", "g"]
             for c in dig_dict[digits[curr_dig]]:
                 find_str(p_str + c, curr_dig+1)
             return p_str 
