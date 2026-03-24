14d776a835c6721d47f4ac4b34fa282084e4a824
chenedwin54288 <109391763+chenedwin54288@users.noreply.github.com>
2026-03-24 17:59:51 +0100

    Time: 367 ms (31.54%) | Memory: 13 MB (88.17%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -5,41 +5,56 @@ class Solution(object):
         :type word2: str
         :rtype: bool
         """
-        # check length same
+        # # check length same
+        # if len(word1) != len(word2):
+        #     return False
+        
+        # # apply op1 many times
+        # word1 = sorted(word1)
+        # word2 = sorted(word2)
+        # if word1 == word2:
+        #     return True
+
+        # # make a map so that op2 can be applied
+        # word1_map = {}
+        # word2_map = {}
+        # for i,j in zip(word1, word2):
+        #     word1_map[i] = 1 + word1_map.get(i, 0)
+        #     word2_map[j] = 1 + word2_map.get(j, 0)
+
+        
+        # freq_list2 = []
+        # for cr2, target_ct2 in word2_map.items():
+        #     if cr2 in word1_map:
+        #         # for cr1, target_ct1 in word1_map.items():
+        #         #     if target_ct2 == target_ct1:
+        #         #         temp = word1_map[cr2] 
+        #         #         word1_map[cr1] = temp
+        #         #         word1_map[cr2] = target_ct1
+        #         freq_list2.append(target_ct2)
+        #     else:
+        #         return False
+
+        # if sorted(list(word1_map.values())) != sorted(freq_list2):
+        #     # print(word1_map, word2_map)
+        #     return False
+        
+        # return True
         if len(word1) != len(word2):
             return False
         
-        # apply op1 many times
-        word1 = sorted(word1)
-        word2 = sorted(word2)
-        if word1 == word2:
-            return True
-
-        # make a map so that op2 can be applied
-        word1_map = {}
-        word2_map = {}
-        for i,j in zip(word1, word2):
-            word1_map[i] = 1 + word1_map.get(i, 0)
-            word2_map[j] = 1 + word2_map.get(j, 0)
-
+        # Count frequencies
+        c1 = Counter(word1)
+        c2 = Counter(word2)
         
-        freq_list2 = []
-        for cr2, target_ct2 in word2_map.items():
-            if cr2 in word1_map:
-                # for cr1, target_ct1 in word1_map.items():
-                #     if target_ct2 == target_ct1:
-                #         temp = word1_map[cr2] 
-                #         word1_map[cr1] = temp
-                #         word1_map[cr2] = target_ct1
-                freq_list2.append(target_ct2)
-            else:
-                return False
-
-        if sorted(list(word1_map.values())) != sorted(freq_list2):
-            # print(word1_map, word2_map)
+        # Rule 1: Must have the same unique characters
+        # Using set() or checking keys() works here
+        if set(c1.keys()) != set(c2.keys()):
             return False
-        
-        return True
+            
+        # Rule 2: The frequencies themselves must be the same 
+        # (e.g., if one has frequencies 3, 2, 1, the other must too)
+        return sorted(c1.values()) == sorted(c2.values())
 
         
 
