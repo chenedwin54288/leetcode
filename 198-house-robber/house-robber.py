ad1d3dbbfb3236a8381dfdd8cf8a43830fc3897f
chenedwin54288 <109391763+chenedwin54288@users.noreply.github.com>
2026-04-01 01:29:35 +0200

    Time: 0 ms (100.00%) | Memory: 12.2 MB (94.07%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -4,9 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # The way to think about this question is very similar to LC746
         # [2, 1, 1] 0 -> 2
+        #           i and i+2
         # [2, 1, 1, 2] 0 -> 3
+        #           i and i+3
         # [2, 1, 1, 1, 2] 0 -> 2 -> 4
+        #           no reason to check i+4 as we can do i->i+2->i+4 (more robbing)
+        #                                            or i+1->i+3->i+5
 
         # [2,1,1,2]
         # i = 3, rob[3] = nums[3] + max(rob[5], rob[6]) 
@@ -21,7 +26,7 @@ class Solution(object):
         rob = [0] * (len(nums) + 3)
         for i in range(len(nums)-1, -1, -1):
             rob[i] = nums[i] + max(rob[i+2], rob[i+3])
-            
+
         return max(rob[0], rob[1])
 
 
