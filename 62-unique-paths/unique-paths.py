be0857f6532c65491499350fe0b30866f422d9f6
chenedwin54288 <109391763+chenedwin54288@users.noreply.github.com>
2026-04-02 10:52:22 +0200

    Time: 4 ms (12.97%) | Memory: 12.3 MB (95.44%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        solu = [[0]*n]*m
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or j == 0:
+                    solu[i][j] = 1
+                else:
+                    solu[i][j] = solu[i][j-1] + solu[i-1][j]
+        
+        return solu[i][j]
+       
+
+        
+        
+        
